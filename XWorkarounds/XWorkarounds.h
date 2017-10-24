6d0d1170cfa6f4f62fae89921ff6f63fb2338136
tarunon <tarunon@mercari.com>
2017-10-24 15:16:39 +0900

    Initial Commit

---
+++ XWorkarounds/XWorkarounds.h
@@ -0,0 +1,19 @@
+//
+//  XWorkarounds.h
+//  XWorkarounds
+//
+//  Created by tarunon on 2017/10/24.
+//  Copyright © 2017 tarunon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XWorkarounds.
+FOUNDATION_EXPORT double XWorkaroundsVersionNumber;
+
+//! Project version string for XWorkarounds.
+FOUNDATION_EXPORT const unsigned char XWorkaroundsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWorkarounds/PublicHeader.h>
+
+
