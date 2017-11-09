eb6993f50c3b12dc1ef743fc8cf1e1bf02213431
tarunon <tarunon@mercari.com>
2017-11-09 19:28:54 +0900

    remove unused file

--- XWorkaroundsExample/SafeAreaVisibleViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SafeAreaVisibleViewController.swift
-//  XWorkaroundsExample
-//
-//  Created by tarunon on 2017/10/24.
-//  Copyright © 2017 tarunon. All rights reserved.
-//
-
-import UIKit
